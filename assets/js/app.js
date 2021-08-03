09a75fd7063c1e982339742453300c7a902eca67
Kwathuta <kelvin.gitahi@student.moringaschool.com>
2021-08-03 12:16:01 +0300

    linked js and created constructor

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+pizzas = [];
+
+function PizzaItem(size, toppings, crust, individualCost) {
+    this.sizeName = size,
+    this.toppingsType = toppings,
+    this.crustName = crust,
+    this.cost = individualCost
+}
+
+
+
+let options, size, crust, toppings;
